9f6e94ac2f8ee8629a75b532d02c887c2aa99712
Meeyad Mohd. Shabab <ms@fourc.eu>
2020-11-07 10:11:45 +0100

    social login added || TODO: create user profile page

--- src/features/auth/authReducer.js
+++ src/features/auth/authReducer.js
@@ -13,7 +13,7 @@ export default function authReducer(state = initialState, { type, payload }) {
         authenticated: true,
         currentUser: {
           email: payload.email,
-          photoURL: payload.photoURL,
+          photoURL: payload.photoURL || "/assets/user.png",
         },
       };
 
