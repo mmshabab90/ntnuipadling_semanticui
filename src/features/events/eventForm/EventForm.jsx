24bf62c91e8a926cddc57dd4113a9f6a4406f850
Meeyad Shabab <ms@fourc.eu>
2021-04-28 20:48:19 +0200

    password reset feature included

--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -140,7 +140,7 @@ export default function EventForm({ match, history, location }) {
 
             <MyTextInput
               name="total_participants"
-              placeholder="Total Participants"
+              placeholder="Total Participants (Event Creator is added as an attendee by default)"
               label="Total Participants"
             />
 
